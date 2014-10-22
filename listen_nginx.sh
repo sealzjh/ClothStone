1ad2d6e9c7b7bea00b68cd66b5b07d89ce0110a3
alanzhu <jjx86@126.com>
2014-10-22 18:20:21 +0800

    fix send mail

--- listen_nginx.sh
+++ listen_nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DADA_ERROR_LOG=/home/alan/test.log
+DADA_ERROR_LOG=/var/log/nginx/dada_error_log
 LAST_LINE_LOG=/tmp/nginxerrorlinenum.log
 ONE_SECOND_LOG=/tmp/tmpnginxerror.log
 MAIL_SEND_PYFILE=/home/ubuntu/potter/mail_send.py
@@ -8,24 +8,23 @@ RECEIVER="tech@imdada.cn"
 CURRENT_TIME=`date "+%Y-%m-%d %H:%M:%S"`
 
 if [ -f "$DADA_ERROR_LOG" ];then
-	LAST_LINENUM=`cat $LAST_LINE_LOG`
-	CURRENT_LINENUM=`wc -l $DADA_ERROR_LOG|awk '{print $1}'`
-	echo $CURRENT_LINENUM > $LAST_LINE_LOG
-	TAIL_OFFSET=$((CURRENT_LINENUM - LAST_LINENUM))
+    LAST_LINENUM=`cat $LAST_LINE_LOG`
+    CURRENT_LINENUM=`wc -l $DADA_ERROR_LOG|awk '{print $1}'`
+    echo $CURRENT_LINENUM > $LAST_LINE_LOG
+    TAIL_OFFSET=$((CURRENT_LINENUM - LAST_LINENUM))
 
-	if [ $TAIL_OFFSET -gt 0 ];then
-		echo $TAIL_OFFSET
-		tail $DADA_ERROR_LOG -n$TAIL_OFFSET > $ONE_SECOND_LOG
-		
-		ERROR_NUM=`cat $ONE_SECOND_LOG |wc -l`
-		ERROR_LOG=`cat $ONE_SECOND_LOG |awk '{print $0}'`
+    if [ $TAIL_OFFSET -gt 0 ];then
+        echo $TAIL_OFFSET
+        tail $DADA_ERROR_LOG -n$TAIL_OFFSET > $ONE_SECOND_LOG
 
-		if [ $ERROR_NUM -gt 0 ];then
-			SUBJECT="Nginx error警告"
-			CONTENT=${CURRENT_TIME}" 线上Nginx errpr : "${ERROR_NUM}"个<br>"${ERROR_LOG}
-			python $MAIL_SEND_PYFILE "$RECEIVER" "$SUBJECT" "$CONTENT"
+        ERROR_NUM=`cat $ONE_SECOND_LOG |wc -l`
+        ERROR_LOG=`cat $ONE_SECOND_LOG |awk 'gsub(/"/ , "\"" , $0) {print $0}'`
 
-		fi
-	fi
+        if [ $ERROR_NUM -gt 0 ];then
+            SUBJECT="Nginx error警告"
+            CONTENT=${CURRENT_TIME}" Nginx error : "${ERROR_NUM}"<br><br>"${ERROR_LOG}
+            python $MAIL_SEND_PYFILE "$RECEIVER" "$SUBJECT" "$CONTENT"
 
+        fi
+    fi
 fi
