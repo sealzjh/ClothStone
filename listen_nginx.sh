387886291e89f3a8dbb4d2d6f8f2df8480a74fe5
alan <alan@Server.lan>
2014-10-22 17:48:08 +0800

    listen nginx

---
+++ listen_nginx.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+DADA_ERROR_LOG=/home/alan/test.log
+LAST_LINE_LOG=/tmp/nginxerrorlinenum.log
+ONE_SECOND_LOG=/tmp/tmpnginxerror.log
+MAIL_SEND_PYFILE=/home/ubuntu/potter/mail_send.py
+RECEIVER="tech@imdada.cn"
+
+CURRENT_TIME=`date "+%Y-%m-%d %H:%M:%S"`
+
+if [ -f "$DADA_ERROR_LOG" ];then
+	LAST_LINENUM=`cat $LAST_LINE_LOG`
+	CURRENT_LINENUM=`wc -l $DADA_ERROR_LOG|awk '{print $1}'`
+	echo $CURRENT_LINENUM > $LAST_LINE_LOG
+	TAIL_OFFSET=$((CURRENT_LINENUM - LAST_LINENUM))
+
+	if [ $TAIL_OFFSET -gt 0 ];then
+		echo $TAIL_OFFSET
+		tail $DADA_ERROR_LOG -n$TAIL_OFFSET > $ONE_SECOND_LOG
+		
+		ERROR_NUM=`cat $ONE_SECOND_LOG |wc -l`
+		ERROR_LOG=`cat $ONE_SECOND_LOG |awk '{print $0}'`
+
+		if [ $ERROR_NUM -gt 0 ];then
+			SUBJECT="Nginx error警告"
+			CONTENT=${CURRENT_TIME}" 线上Nginx errpr : "${ERROR_NUM}"个<br>"${ERROR_LOG}
+			python $MAIL_SEND_PYFILE "$RECEIVER" "$SUBJECT" "$CONTENT"
+
+		fi
+	fi
+
+fi
